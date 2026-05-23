3dfdf1b6c0c9761d168f4d2c446eff2f50de1c3f
NihithaT <nihal.thombarapu@gmail.com>
2026-05-23 18:08:48 +0200

    Add files via upload

---
+++ Print Statements and Comments.py
@@ -0,0 +1,13 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Nihitha.")
+
+# Printing a fun fact about myself
+# print("I love doing kungfu.")
+print("I love doing kungfu.") 
+
+# Performing and printing a calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
